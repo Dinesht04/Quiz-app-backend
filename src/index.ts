2a89ce871bcb9806828c4f2bf7bd6cf071b857b1
Dinesh <dineshtyagi567@gmail.com>
2025-07-17 03:11:28 +0530

    Express-ws Integration

--- src/index.ts
+++ src/index.ts
@@ -5,283 +5,296 @@ import dotenv from 'dotenv';
 import { message, Question, QuizRoom } from './types';
 import { CallGemini } from './gemini';
 import { removeClientFromRooms } from './RemoveClientFromRoom';
+
 dotenv.config();
+
+var express = require('express')
+
+const app = express();
+var expressWs = require('express-ws')(app);
+
+export const rooms: { [roomId: string]: QuizRoom } = {};
+
+let userCount = 0;
+
 
 // @ts
 const PORT: number = parseInt(<string>process.env.PORT, 10) || 3000
 
-export const rooms: { [roomId: string]: QuizRoom } = {};
+app.get('/', function(req:Request, res:Response){
+  console.log('get route');
+});
 
-if (PORT) {
-  const wss = new WebSocketServer({ port: PORT });
-  console.log(`WebSocket server started on ws://localhost:${PORT}`);
+app.ws('/', function(ws :WebSocket, req:Request) {
 
-  let userCount = 0;
-
-  wss.on('connection', (ws: WebSocket) => {
+  ws.on('open',(ws: WebSocket)=>{
     console.log('Client connected!');
     userCount++;
-    ws.on('message', async (message) => {
-      const msgStr: string = message.toString();
-      const msg: message = JSON.parse(msgStr);
+  })
 
-      const expires = msg.payload.expires;
-      const roomId = msg.payload.roomId;
+  ws.on('message', async (message) => {
+    const msgStr: string = message.toString();
+    const msg: message = JSON.parse(msgStr);
 
-      if (new Date(expires) > new Date()) {
-        console.log('BRo is autheticated');
-      }
 
-      if (new Date(expires) < new Date()) {
-        console.log('BRo is not autheticated');
-        ws.close(); // or reject auth
-        return;
-      }
 
-      if (msg.type === 'join') {
-        const username = msg.payload.username;
+    const expires = msg.payload.expires;
+    const roomId = msg.payload.roomId;
 
-        if (!rooms[roomId]) {
-          rooms[roomId] = {
-            clients: new Set<WebSocket>([ws]),
-            host: { username: username, Websocket: ws },
-            questions: null,
-            scores: new Map(),
-            answered: new Map([[username, 0]]), // tracks which question IDs user has answered
-            state: 'waiting',
-            clientInfo: new Map([[ws, { name: username }]]),
-          };
-          console.log(
-            `USer joined but didnt exist, so Created new room: ${roomId}`,
-          );
-          ws.send(
-            JSON.stringify({
-              type: 'join',
-              status: 'successful',
-              host: true,
-            }),
-          );
-        } else {
-          rooms[roomId].clients.add(ws); // Add socket to the room
-          rooms[roomId].clientInfo.set(ws, { name: username });
-          rooms[roomId].answered.set(username, 0);
+    if (new Date(expires) > new Date()) {
+      console.log('BRo is autheticated');
+    }
 
-          console.log(username, ' Joined room', roomId);
-          ws.send(
-            JSON.stringify({
-              type: 'join',
-              status: 'successful',
-              host: false,
-            }),
-          );
-        }
+    if (new Date(expires) < new Date()) {
+      console.log('BRo is not autheticated');
+      ws.close(); // or reject auth
+      return;
+    }
 
-        const clientList = Array.from(rooms[roomId].clientInfo.values()).map(
-          (client) => client.name,
-        );
+    if (msg.type === 'join') {
+      const username = msg.payload.username;
 
-        rooms[roomId].clients.forEach((socket) => {
-          socket.send(
-            JSON.stringify({
-              type: 'client-list',
-              payload: clientList,
-            }),
-          );
-        });
-      }
-
-      if (msg.type === 'message') {
-        const username = msg.payload.username;
-
-        const Messagepayload = {
-          type: 'message',
-          payload: {
-            message: msg.payload.message,
-            username: username,
-            time: msg.payload.time,
-          },
+      if (!rooms[roomId]) {
+        rooms[roomId] = {
+          clients: new Set<WebSocket>([ws]),
+          host: { username: username, Websocket: ws },
+          questions: null,
+          scores: new Map(),
+          answered: new Map([[username, 0]]), // tracks which question IDs user has answered
+          state: 'waiting',
+          clientInfo: new Map([[ws, { name: username }]]),
         };
-        rooms[roomId].clients.forEach((socket) => {
-          socket.readyState === WebSocket.OPEN
-            ? socket.send(JSON.stringify(Messagepayload))
-            : null;
-        });
-      }   
-
-      if (msg.type === 'leave') {
-        removeClientFromRooms(ws);
+        console.log(
+          `USer joined but didnt exist, so Created new room: ${roomId}`,
+        );
         ws.send(
           JSON.stringify({
-            type: 'leave',
+            type: 'join',
             status: 'successful',
+            host: true,
+          }),
+        );
+      } else {
+        rooms[roomId].clients.add(ws); // Add socket to the room
+        rooms[roomId].clientInfo.set(ws, { name: username });
+        rooms[roomId].answered.set(username, 0);
+
+        console.log(username, ' Joined room', roomId);
+        ws.send(
+          JSON.stringify({
+            type: 'join',
+            status: 'successful',
+            host: false,
           }),
         );
       }
 
-      //ROOM STARTING
-      if (msg.type === 'start') {
-        try {
-          if (rooms[roomId].host.Websocket !== ws) {
-            const message = {
-              type: 'unauthorised',
-              payload: {
-                message: 'Only the host can start the quiz',
-              },
-            };
+      const clientList = Array.from(rooms[roomId].clientInfo.values()).map(
+        (client) => client.name,
+      );
 
-            ws.send(JSON.stringify(message));
-          } else {
-            const difficulty = msg.payload.difficulty;
-            const geminiResponse: Question[] = await CallGemini(
-              msg.payload.topic,
-              difficulty,
-            );
-            console.log(geminiResponse);
-
-            rooms[roomId].questions = geminiResponse;
-            rooms[roomId].state = 'in-progress';
-
-            const payload = {
-              type: 'questions',
-              payload: geminiResponse,
-            };
-
-            rooms[roomId].clients.forEach((socket) => {
-              socket.readyState === WebSocket.OPEN
-                ? socket.send(JSON.stringify(payload))
-                : null;
-            });
-            console.log('ROom started');
-          }
-        } catch (err) {
-          console.log('Error in gemini response', err);
-        }
-      }
-      //CHECKING QUIZ ANSWER
-      if (msg.type === 'answer') {
-        const QuestionId = msg.payload.QuestionId;
-        const Answer = msg.payload.Answer;
-        const username = msg.payload.username;
-
-        const question = rooms[roomId].questions?.find(
-          (q) => q.id === QuestionId,
+      rooms[roomId].clients.forEach((socket) => {
+        socket.send(
+          JSON.stringify({
+            type: 'client-list',
+            payload: clientList,
+          }),
         );
+      });
+    }
 
-        let currentScore = rooms[roomId].scores?.get(ws)?.score;
+    if (msg.type === 'message') {
+      const username = msg.payload.username;
 
-        if (!currentScore) {
-          rooms[roomId].scores?.set(ws, { username: username, score: 0 });
-          currentScore = 0;
+      const Messagepayload = {
+        type: 'message',
+        payload: {
+          message: msg.payload.message,
+          username: username,
+          time: msg.payload.time,
+        },
+      };
+      rooms[roomId].clients.forEach((socket) => {
+        socket.readyState === WebSocket.OPEN
+          ? socket.send(JSON.stringify(Messagepayload))
+          : null;
+      });
+    }   
+
+    if (msg.type === 'leave') {
+      removeClientFromRooms(ws);
+      ws.send(
+        JSON.stringify({
+          type: 'leave',
+          status: 'successful',
+        }),
+      );
+    }
+
+    //ROOM STARTING
+    if (msg.type === 'start') {
+      try {
+        if (rooms[roomId].host.Websocket !== ws) {
+          const message = {
+            type: 'unauthorised',
+            payload: {
+              message: 'Only the host can start the quiz',
+            },
+          };
+
+          ws.send(JSON.stringify(message));
+        } else {
+          const difficulty = msg.payload.difficulty;
+          const geminiResponse: Question[] = await CallGemini(
+            msg.payload.topic,
+            difficulty,
+          );
+          console.log(geminiResponse);
+
+          rooms[roomId].questions = geminiResponse;
+          rooms[roomId].state = 'in-progress';
+
+          const payload = {
+            type: 'questions',
+            payload: geminiResponse,
+          };
+
+          rooms[roomId].clients.forEach((socket) => {
+            socket.readyState === WebSocket.OPEN
+              ? socket.send(JSON.stringify(payload))
+              : null;
+          });
+          console.log('ROom started');
         }
-        if (question) {
-          const isCorrect = question.correct === Answer;
-          if (isCorrect) {
-            const payload = {
-              type: 'answer',
-              payload: {
-                QuestionId: QuestionId,
-                Correct: true,
-              },
-            };
-            ws.send(JSON.stringify(payload));
+      } catch (err) {
+        console.log('Error in gemini response', err);
+      }
+    }
+    //CHECKING QUIZ ANSWER
+    if (msg.type === 'answer') {
+      const QuestionId = msg.payload.QuestionId;
+      const Answer = msg.payload.Answer;
+      const username = msg.payload.username;
 
-            console.log('Correct ans:', Answer);
-            rooms[roomId].scores?.set(ws, {
-              username: username,
-              score: currentScore + 1,
+      const question = rooms[roomId].questions?.find(
+        (q) => q.id === QuestionId,
+      );
+
+      let currentScore = rooms[roomId].scores?.get(ws)?.score;
+
+      if (!currentScore) {
+        rooms[roomId].scores?.set(ws, { username: username, score: 0 });
+        currentScore = 0;
+      }
+      if (question) {
+        const isCorrect = question.correct === Answer;
+        if (isCorrect) {
+          const payload = {
+            type: 'answer',
+            payload: {
+              QuestionId: QuestionId,
+              Correct: true,
+            },
+          };
+          ws.send(JSON.stringify(payload));
+
+          console.log('Correct ans:', Answer);
+          rooms[roomId].scores?.set(ws, {
+            username: username,
+            score: currentScore + 1,
+          });
+        } else {
+          const payload = {
+            type: 'answer',
+            payload: {
+              QuestionId: QuestionId,
+              Correct: false,
+            },
+          };
+          ws.send(JSON.stringify(payload));
+          console.log('InCorrect ans:', Answer);
+        }
+
+        let questionsAnswered = rooms[roomId].answered.get(username);
+
+        if (!questionsAnswered) {
+          questionsAnswered = 0;
+        }
+
+        rooms[roomId].answered.set(username, questionsAnswered + 1);
+
+        console.log(Array.from(rooms[roomId].answered.entries()));
+        const liveScore = Array.from(rooms[roomId].answered.entries());
+        rooms[roomId].clients.forEach((socket) => {
+          socket.send(
+            JSON.stringify({
+              type: 'live-score',
+              payload: {
+                liveScore: liveScore,
+              },
+            }),
+          );
+        });
+      } else {
+        console.error('Question not found.');
+      }
+    }
+
+    if (msg.type === 'finish') {
+      console.log('qUIZ FINISHED for user');
+
+      if (rooms[roomId].scores) {
+        let over = true;
+
+        for (const score of rooms[roomId].answered) {
+          if (score[1] < 5) {
+            over = false;
+            break;
+          }
+        }
+
+        if (over) {
+          const finalScoreList: { username: string; score: number }[] = [];
+
+          rooms[roomId].scores?.forEach((score) => {
+            finalScoreList.push({
+              username: score.username,
+              score: score.score,
             });
-          } else {
-            const payload = {
-              type: 'answer',
-              payload: {
-                QuestionId: QuestionId,
-                Correct: false,
-              },
-            };
-            ws.send(JSON.stringify(payload));
-            console.log('InCorrect ans:', Answer);
-          }
+          });
 
-          let questionsAnswered = rooms[roomId].answered.get(username);
-
-          if (!questionsAnswered) {
-            questionsAnswered = 0;
-          }
-
-          rooms[roomId].answered.set(username, questionsAnswered + 1);
-
-          console.log(Array.from(rooms[roomId].answered.entries()));
-          const liveScore = Array.from(rooms[roomId].answered.entries());
           rooms[roomId].clients.forEach((socket) => {
             socket.send(
               JSON.stringify({
-                type: 'live-score',
+                type: 'final-score',
                 payload: {
-                  liveScore: liveScore,
+                  finalScores: finalScoreList,
                 },
               }),
             );
           });
-        } else {
-          console.error('Question not found.');
+
+          rooms[roomId].state = 'ended';
         }
+      } else {
+        ws.send(
+          JSON.stringify({
+            type: 'error',
+            payload: {
+              message: 'No Scores available for this room',
+            },
+          }),
+        );
       }
+    }
+  })
 
-      if (msg.type === 'finish') {
-        console.log('qUIZ FINISHED for user');
+  
+});
 
-        if (rooms[roomId].scores) {
-          let over = true;
+app.listen(PORT,()=>{
+  console.log('Listening on PORT:',PORT)
+});
 
-          for (const score of rooms[roomId].answered) {
-            if (score[1] < 5) {
-              over = false;
-              break;
-            }
-          }
 
-          if (over) {
-            const finalScoreList: { username: string; score: number }[] = [];
 
-            rooms[roomId].scores?.forEach((score) => {
-              finalScoreList.push({
-                username: score.username,
-                score: score.score,
-              });
-            });
-
-            rooms[roomId].clients.forEach((socket) => {
-              socket.send(
-                JSON.stringify({
-                  type: 'final-score',
-                  payload: {
-                    finalScores: finalScoreList,
-                  },
-                }),
-              );
-            });
-
-            rooms[roomId].state = 'ended';
-          }
-        } else {
-          ws.send(
-            JSON.stringify({
-              type: 'error',
-              payload: {
-                message: 'No Scores available for this room',
-              },
-            }),
-          );
-        }
-      }
-    });
-
-    // Event listener for when the connection with this specific client is closed.
-    ws.on('close', () => {
-      removeClientFromRooms(ws);
-      console.log('Client disconnected.');
-    });
-  });
-} else {
-  console.log('Error: PORT Undefined');
-}
